adb6b5bf12031c789cd4462e1f76c68f81b194ab
sedeuu <sedeuu@hotmail.com>
2020-12-31 01:53:48 -0500

    Update hatemath.py

--- coding/hatemath.py
+++ coding/hatemath.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 import time
 import getpass
 
