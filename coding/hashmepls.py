4cc5f63940fc6824058c292e179b71600174eb0c
sedeuu <sedeuu@hotmail.com>
2021-01-01 20:14:04 -0500

    Update hashmepls.py

--- coding/hashmepls.py
+++ coding/hashmepls.py
@@ -21,6 +21,5 @@ time.sleep(1)
 s = driver.find_element_by_class_name("message")
 e = s.text
 e = e[e.find('\n')+1:e.rfind('\n')]
-h = hashlib.sha512(e.encode("utf-8"))
-new_url = h.hexdigest()
-driver.get(url + new_url)
+h = hashlib.sha512(e.encode("utf-8")).hexdigest()
+driver.get(url + h)
